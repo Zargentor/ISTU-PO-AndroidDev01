c9234477e921f3e7b554fa5fed1fd6d041000559
Mattew <Zarger89@gmail.com>
2025-02-04 22:53:48 +0400

    Добавил 4 картинки, сделал динамический показ картинок, добавил новый вид пива

--- app/src/main/java/com/example/beeradviser/MainActivity.kt
+++ app/src/main/java/com/example/beeradviser/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.beeradviser
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -13,23 +14,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val findBeer = findViewById<Button>(R.id.find_beer)
+        val beerColorSpinner = findViewById<Spinner>(R.id.beer_color)
+        val brandsTextView = findViewById<TextView>(R.id.brands)
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+
         findBeer.setOnClickListener {
-            val beerColor = findViewById<Spinner>(R.id.beer_color)
-            val color = beerColor.selectedItem
-            val beerList = getBeers(color.toString())
-            val beers = beerList.reduce { str, item -> str + '\n' + item }
-            val brands = findViewById<TextView>(R.id.brands)
-            brands.text = beers
+
+            val selectedColor = beerColorSpinner.selectedItem.toString()
+            val beerList = getBeers(selectedColor)
+            val beerNames = beerList.joinToString(separator = "\n")
+
+            brandsTextView.text = beerNames
+
+            updateImages(selectedColor, imageView1, imageView2)
         }
     }
+}
 
-    fun getBeers(color: String): List<String> {
-        return when (color) {
-            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
-            "Amber" -> listOf("Jack Amber", "Red Moose")
-            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
-            else -> listOf("Gout Stout", "Dark Daniel")
+fun getBeers(color: String): List<String> {
+    return when (color) {
+        "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+        "Amber" -> listOf("Jack Amber", "Red Moose")
+        "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+        "Red" -> listOf("Tomato pepper beer", "Red Sea Ale")
+        else -> listOf("Gout Stout", "Dark Daniel")
+    }
+}
+
+fun updateImages(color: String, imageView1: ImageView, imageView2: ImageView) {
+    when (color) {
+        "Light" -> {
+            imageView1.setImageResource(R.drawable.lager)
+            imageView2.setImageResource(R.drawable.ipa)
+        }
+
+        "Amber" -> {
+            imageView1.setImageResource(R.drawable.amber)
+            imageView2.setImageResource(R.drawable.redmoose)
         }
     }
 }
